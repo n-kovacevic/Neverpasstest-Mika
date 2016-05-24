0fd7a4de8f71f287ad7ac4112787f539b9dba21d
Nikola Kovačević <nkg5.rs@gmail.com>
2016-05-24 21:59:04 +0200

    Naming bug, and no scores file bug fixed. Fixed a bug that occurred after file name changes and bug which caused scores not to be loaded if file scores was not existing.

--- game.py
+++ game.py
@@ -1,15 +1,15 @@
-import pygame, sys
+import pygame
+import sys
 import score_manager
 from pygame.locals import *
-
 import state
 
 
 def main():
     pygame.init()
-    screen = pygame.display.set_mode((640,480))
+    screen = pygame.display.set_mode((640, 480))
     pygame.display.set_caption("Neverpasstest Mika")
-    state = 0
+    current_state = 0
     menu = state.Menu(screen)
     game = None
     hs = None
@@ -22,25 +22,25 @@ def main():
             if event.type == QUIT:
                 pygame.quit()
                 sys.exit()
-        if state == 0:
+        if current_state == 0:
             if menu.update():
                 game = state.Game(screen)
-                state = 1
-        elif state == 1:
+                current_state = 1
+        elif current_state == 1:
             score = game.update()
             if score >= 0:
                 if score_manager.is_highscore(score):
                     hs = state.HighScore(score, screen)
-                    state = 2
+                    current_state = 2
                 else:
                     menu = state.Menu(screen)
-                    state = 0
-        elif state == 2:
+                    current_state = 0
+        elif current_state == 2:
             score = hs.update()
             if score:
                 score_manager.add_score(score)
                 menu = state.Menu(screen)
-                state = 0
+                current_state = 0
         pygame.display.update()
         clock.tick(30)
 
